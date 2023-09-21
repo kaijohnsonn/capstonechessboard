970d23203a2171f6be3ab7d8cab5e97d472b896e
Katie Nikolov <nikolov.k@northeastern.edu>
2023-09-21 14:26:12 -0400

    updated api token

--- LIChessAPI.py
+++ LIChessAPI.py
@@ -1,10 +1,12 @@
 import berserk
 import requests
 
-session = berserk.TokenSession('lip_JMyZWpXAFOfLefFVXf4Z')
+
+session = berserk.TokenSession('lip_UyYinDogVSzglgIhis7O')
 client = berserk.Client(session=session)
 response_API = requests.get('https://lichess.org')
 # confirms API call
-# print(client.users.get_public_data('nucapstonet4'))
+print(client.users.get_public_data('nucapstonet4'))
 print(client.account.get())
 
+client.challenges.create_ai()
