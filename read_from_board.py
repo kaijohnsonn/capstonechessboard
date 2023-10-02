ba749d28cbffe5b6616caeff02210fb9047b17c2
smoyer <moyer.s@northeastern.edu>
2023-10-02 10:42:29 -0400

    working through creating chess notation from entry

--- read_from_board.py
+++ read_from_board.py
@@ -9,20 +9,43 @@ king: "K" queen: "Q" rook: "R" bishop: "B" knight: "N"
 (ex: 2.Nf3)
 pawn you see only the name of the square where the pawn moves.
 (ex: 1.e4)
+king is threatened: "+" [end of string]
+checkmate: "#" [end of string]
+- Kingside Castling: O-O
+- Queenside Castling: O-O-O
+For capturing, put an “x” between piece moving and square moved to
+Pawn promotion: [file][row] + "=" + [new-piece-code]
+* files: a-h and ranks: 1-8.
 """
-
+# convert the move made by player into chess notation
+# check for castling [o-o] or [o-o-o] 
+# [piece-code]+[from-file]+["x"]?+[to-file]+[to-row] + ["+"/"#""]
+def chess_notation():
+    
+    return ""
 
 
 
 button_pins = [1, 2]  # Example GPIO pins for two buttons
-rfid_scanners = [SimpleMFRC522(0), SimpleMFRC522(1),SimpleMFRC522(2), SimpleMFRC522(3)]  # Initialize two RFID scanners
+rfid_scanners = [SimpleMFRC522(0), SimpleMFRC522(1),SimpleMFRC522(2), SimpleMFRC522(3)]  # Initialize four RFID scanners
 
-def on_button_pressed(button_id):
-    print(f"Button {button_id} pressed.")
-    
-    read_rfids()
+# user presses button signifying end of their turn
+def on_user_button_press():
+        print(f"Button {button_id} pressed.")  
+        read_rfids()
 
+#user presses button signifying they made the computer/other player's move
+def on_comp_button_press():
+         print(f"Button {button_id} pressed.")
+         # start user's move timer
 
+# read from sensors and detect changes from previous move 
+"""
+RFID tags should differentiate - 
+king: "K" queen: "Q" rook: "R" bishop: "B" knight: "N"
+RFID scanners should be assigned position (ex: e5)
+
+"""
 def read_rfids():
     for idx, scanner in enumerate(rfid_scanners):
         print(f"Scanning RFID scanner {idx}...")
@@ -31,3 +54,5 @@ def read_rfids():
             print(f"RFID scanner {idx} - Card ID: {id}, Data: {text}")
         except Exception as e:
             print(f"Error reading from RFID scanner {idx}: {str(e)}")
+
+
