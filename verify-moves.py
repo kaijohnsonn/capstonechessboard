d8087ebed5d0148146cddd35532ac16786a7591b
smoyer <moyer.s@northeastern.edu>
2023-12-02 16:52:27 -0500

    verify moves added wip

---
+++ verify-moves.py
@@ -0,0 +1,9 @@
+def verify_opp_move():
+    while True:
+        if but2_pressed[0]:
+            but2_pressed[0] = False  # Reset the flag
+            # CHECK IF THE MOVE WAS CORRECT
+            
+            break
+
+        time.sleep(0.1)
